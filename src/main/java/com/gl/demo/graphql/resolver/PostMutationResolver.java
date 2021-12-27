4c94ebe301e1bf85487083537b3b8887c27627f0
Abel Acuña <abel.acuna@globallogic.com>
2021-12-27 02:42:39 -0300

    Added Mutation to create Posts

---
+++ src/main/java/com/gl/demo/graphql/resolver/PostMutationResolver.java
@@ -0,0 +1,25 @@
+package com.gl.demo.graphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.gl.demo.graphql.dto.PostDTO;
+import com.gl.demo.graphql.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class PostMutationResolver implements GraphQLMutationResolver {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostMutationResolver(PostService postService) {
+        this.postService = postService;
+    }
+
+    public UUID createPost(PostDTO post) {
+        return this.postService.createPost(post);
+    }
+
+}
--- src/main/java/com/gl/demo/graphql/service/PostService.java
+++ src/main/java/com/gl/demo/graphql/service/PostService.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 public interface PostService {
     List<PostDTO> getPostsByAuthorId(UUID authorId);
     List<PostDTO> getRecentPosts(int count, int offset);
+    UUID createPost(PostDTO post);
 }
